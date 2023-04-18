6c500e0a9b0a6c9a60dbd58522115c4385953ffb
unknown <alaa.ahmed20022002@gmail.com>
2023-04-18 17:33:12 +0200

    Update

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,24 +1,21 @@
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXVUwQeSc0MF_5DBgNXmYZHYXVevyu1pE",
-    authDomain: "dashboard-66e7d.firebaseapp.com",
-    projectId: "dashboard-66e7d",
-    storageBucket: "dashboard-66e7d.appspot.com",
-    messagingSenderId: "394644028413",
-    appId: "1:394644028413:web:92bc5d85b7ef83f11af794",
-    measurementId: "G-KXZ9QJ675N"
-  };
+  apiKey: import.meta.env.VITE_API_KEY_AUTH,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT,
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
- export const db = getFirestore(app);
-export const auth=getAuth(app)
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 
-
-export default app
-
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { ContextProvider } from "./Contexts/stateContext";
-import { BrowserRouter  } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
-import { ContextUserProvider } from "./Contexts/UserContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      
-          <ContextProvider>
-     <ThemeProvider>
-    <BrowserRouter> 
-            <App />
+    <ContextProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
         </BrowserRouter>
-    </ThemeProvider>
-          </ContextProvider>
-    
+      </ThemeProvider>
+    </ContextProvider>
   </React.StrictMode>
 );
